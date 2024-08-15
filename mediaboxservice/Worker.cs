eca29d80c7a7ce32b2bf458ac17b129afd9710e6
munibhardwaj <munibhardwaj@MUNIBHARDWAFF0A>
2024-08-15 23:31:01 +0530

    Add project files.

---
+++ mediaboxservice/Worker.cs
@@ -0,0 +1,34 @@
+namespace mediaboxservice
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+        private FileSystemWatcher _watcher;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _watcher = new();
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
